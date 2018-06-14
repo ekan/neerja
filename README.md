caa8761aac45e5f70255cdfe5e12a7f8de6648e6
justinventuri <justinventuri@gmail.com>
2018-06-14 19:11:49 -0400

    twilio-app

--- README.md
+++
@@ -1,2 +0,0 @@
-# neerja
-Neerja's a sassy urban young professional scientist, who’s up on pop culture and quirky/sarcastic, drunk at night, and will always text you back.
--- app.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-const server = http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world!');
-});
-server.listen(process.env.PORT || 8000);
